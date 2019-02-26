68f15469f21efc957037ceb0522e6931cdb6c643
Marc Palacios Domènech <megamarc@hotmail.com>
2019-02-26 19:24:00 +0100

    Updates binding * Project solution upgraded to VisualStudio 2017 * Bundles Tilengine release 2.2.0 * Bundles binding 1.20

--- Tilengine.cs
+++ Tilengine.cs
@@ -1,39 +1,33 @@
 /*
-Tilengine - 2D Graphics library with raster effects
-Copyright (c) 2015-2017 Marc Palacios Domenech (megamarc@hotmail.com)
-All rights reserved.
+Copyright (c) 2018 Marc Palacios Domènech
 
-Redistribution and use in source and binary forms, with or without modification
-are permitted provided that the following conditions are met:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
 /*
 *****************************************************************************
-* C# Tilengine wrapper - Up to date to library version 1.19
+* C# Tilengine wrapper - Up to date to library version 1.20
 * http://www.tilengine.org
 *****************************************************************************
 */
 
 using System;
-using System.IO;
 using System.Runtime.InteropServices;
 
 namespace Tilengine
@@ -891,6 +885,10 @@ namespace Tilengine
 
         [DllImport("Tilengine")]
         [return: MarshalAsAttribute(UnmanagedType.I1)]
+        private static extern bool TLN_SetLayerBitmap(int nlayer, IntPtr bitmap);
+
+        [DllImport("Tilengine")]
+        [return: MarshalAsAttribute(UnmanagedType.I1)]
         private static extern bool TLN_SetLayerPosition(int nlayer, int hstart, int vstart);
 
         [DllImport("Tilengine")]
@@ -1122,6 +1120,18 @@ namespace Tilengine
         /// <summary>
         ///
         /// </summary>
+        public Bitmap Bitmap
+        {
+            set
+            {
+                bool ok = TLN_SetLayerBitmap(index, value.ptr);
+                Engine.ThrowException(ok);
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
         public int Width
         {
             get { return TLN_GetLayerWidth(index); }
@@ -1305,7 +1315,7 @@ namespace Tilengine
         public void Reset()
         {
             bool ok = TLN_ResetSpriteScaling(index);
-            Engine.ThrowException(false);
+            Engine.ThrowException(ok);
         }
 
         /// <summary>
